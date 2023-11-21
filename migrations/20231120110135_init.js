d890c70d4a4da7da05875038b022eaa35bcf0333
kojurkin <kajukinia@stud.omsu.ru>
2023-11-21 13:58:07 +0600

    Commit

--- migrations/20231120110135_init.js
+++ migrations/20231120110135_init.js
@@ -5,22 +5,23 @@
 exports.up = function(knex) {
     return knex.schema
         .createTable('users', function (table){
-            table.increments('id');
-            table.string('login');
-            table.string('hashPassword');
-            table.string('email');
-            table.string('name');
+            table.increments('id').notNullable();
+            table.string('login').notNullable();
+            table.string('hashPassword').notNullable();
+            table.string('email').notNullable();
+            table.string('name').notNullable();
             table.string('avatar');
         })
         .createTable('surveys', function (table){
-            table.increments('id');
-            table.increments('creatorId');
-            table.string('title');
+            table.increments('id').notNullable();
+            table.increments('creatorId').notNullable();
+            table.string('title').notNullable();
         })
         .createTable('surveysRedactors', function (table){
             table.increments('userId');
-            table.increments('surveyId');
+            table.increments('surveyId').notNullable();
         })
+
   
 };
 
